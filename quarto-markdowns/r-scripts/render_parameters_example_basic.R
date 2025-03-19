58ae136cf1d884832b43f90d69e8e49b591b146a
HanleyKingston <hkingsto@gmail.com>
2025-03-19 16:35:38 -0700

    seperate rendering script into basic version and oen with loop

---
+++ quarto-markdowns/r-scripts/render_parameters_example_basic.R
@@ -0,0 +1,19 @@
+################################################
+#
+# Render word markdown for parameters_example.Rmd
+#
+################################################
+
+#clean data environment
+rm(list = ls())
+library(quarto)
+library(dplyr)
+
+
+# Render (using quarto) ----------------------------------------
+quarto::quarto_render(
+  "./quarto-markdowns/paramaters_example.qmd", 
+  output_file = paste0("Report_", Sys.Date(), ".docx"),  #Default output location is working directory
+  execute_params = list(
+    person = "Tom",
+    message = "Get lost")) #we don't provide the exclamation argument, so it will use the default (TRUE)
